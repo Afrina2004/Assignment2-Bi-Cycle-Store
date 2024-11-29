54245b2626243e990015029c319b28e5c15c3c50
Afrina2004 <khanamafrina700@gmail.com>
2024-11-29 19:15:13 +0600

    first commit

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -4,7 +4,7 @@ const productValidationSchema = z.object({
   id: z.string(),
   name: z.string().min(1, "Name is required"),
   brand: z.string().min(1, "Brand is required"),
-  price: z.number().positive("Price must be a positive number"),  // Enforce positive price
+  price: z.number().positive("Price must be a positive number"),  
   type: z.enum(['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric']),
   description: z.string().min(1, "Description is required"),
   quantity: z.number().min(1, "Quantity must be at least 1"),
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import config from './app/config';
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+    console.log("Connected to MongoDB Atlas");
 
     app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
